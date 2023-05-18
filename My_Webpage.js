c74d62dbb976b1afdd1a31899ff3d83ae011d681
rawane diouf <rawanediouf6@gmail.com>
2023-05-18 00:23:01 +0000

    Finishing the first part

--- My_Webpage.js
+++ My_Webpage.js
@@ -1,14 +1,3 @@
-// const modeSombre = document.querySelector('.mode');
-// const modeIcon = document.querySelector('.mode-icon');
-// const body = document.querySelector('body');
-
-// modeSombre.addEventListener('click', () => {
-//     body.classList.toggle('dark-mode');
-//     modeIcon.classList.toggle('bx-moon');
-//     modeIcon.classList.toggle('bx-sun');
-// });
-
-
 /**********background light**********/
 const modeSombre = document.querySelector('.mode-icon');
 
